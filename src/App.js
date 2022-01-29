fc84aaaa6351b199d15149bf04186beb1a353d39
Nguyen Dinh Giang <dinhgiang2611@gmail.com>
2022-01-29 23:27:51 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ class App extends Component {
 
   random () {
     let selected_styles = {}
-    Object.entries(this.props.options).map((obj, index) => {
+    Object.entries(this.props.options).map(obj => {
       selected_styles[obj[0]] =
         obj[1][Math.floor(Math.random() * obj[1].length)]
     })
