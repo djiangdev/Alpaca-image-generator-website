c8f3bb3eb01dba41a8b35db0032bf117700a5389
Nguyen Dinh Giang <dinhgiang2611@gmail.com>
2022-03-05 22:25:40 +0700

    rm auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,13 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "alpacaimagegenerator51aec0a2": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
-    }
-}
+export type AmplifyDependentResourcesAttributes = {}
